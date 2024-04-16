3749d7aae6fd05592484ef23209ee354dc428bef
Laix <894169287@qq.com>
2024-04-16 23:34:50 +0800

    divide render & ui

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { computed, onMounted, onUnmounted, ref } from 'vue';
+import { computed, ref } from 'vue';
 import { Book } from '../modules/indexDb';
-import Drawer from './Drawer.vue';
+import ReaderUI from './ReaderUI.vue';
 
 // 使 v-model 必填
 const model = defineModel({ required: true })
@@ -10,55 +10,9 @@ const props = defineProps<{
 }>()
 
 // 提供一个默认值
-function back() {
+function closeReader() {
     model.value = false
 }
-
-let intervalId = -1
-const limit = ref(false)
-onMounted(() => {
-    console.log('阅读页加载中', props.curBook)
-    autoHide()
-
-    let scrolling = false;
-    document.addEventListener('scroll', () => {
-        scrolling = true
-    })
-    intervalId = setInterval(() => {
-        if (scrolling) {
-            scrolling = false;
-
-            operatePanelVisible.value && changeOperatePanelVisible()
-
-            const { scrollTop, clientHeight, scrollHeight } = document.documentElement
-            if (scrollTop + clientHeight === scrollHeight) {
-                aa('next')
-            } else if (scrollTop === 0 && start.value !== 0) {
-                aa('pre')
-            }
-        }
-    }, 300);
-
-    const intersectionObserver = new IntersectionObserver((entries) => {
-        // 如果 intersectionRatio 为 0，则目标在视野外，
-        // 我们不需要做任何事情。
-        // what if pre & next exist in a viewport?
-        if (entries[0].intersectionRatio <= 0) {
-            console.log('trigger,out')
-            return
-        };
-        console.log("Loaded new items", entries);
-    });
-    // 开始监听
-    const anchor = document.querySelector('#anchor')
-    anchor && intersectionObserver.observe(anchor);
-    setInterval(() => {
-        // alert(intersectionObserver.root)
-    }, 5000)
-})
-onUnmounted(() => {
-    clearInterval(intervalId)
-})
 
 const init = 300
 const gap = 50
@@ -66,69 +20,13 @@ const gap = 50
 const start = ref(0)
 const vList = computed(() => props.curBook.paraArr.slice(start.value, start.value + init))
 
-function aa(type: string) {
-    if (limit.value) return
-    limit.value = true
+function moveWindow(type: string) {
     if (type === 'next') {
         start.value += gap
     } else {
         start.value = gap > start.value ? 0 : start.value - gap
     }
-    setTimeout(() => {
-        limit.value = false
-    }, 0)
 }
-
-const headerVisible = ref(true)
-
-const endSecond = ref(4)
-const curInterval = ref(0)
-function resetEndSecond() {
-    clearInterval(curInterval.value)
-    endSecond.value = 4
-}
-function autoHide() {
-    curInterval.value = setInterval(() => {
-        if (endSecond.value > 0) {
-            endSecond.value -= 1
-        }
-        if (endSecond.value === 0) {
-            changeHeaderVisible()
-        }
-    }, 1000)
-}
-function changeHeaderVisible() {
-    headerVisible.value = !headerVisible.value
-    if (headerVisible.value) {
-        autoHide()
-    } else {
-        resetEndSecond()
-    }
-}
-
-const operatePanelVisible = ref(false)
-function changeOperatePanelVisible() {
-    operatePanelVisible.value = !operatePanelVisible.value
-    if (operatePanelVisible.value) {
-        resetEndSecond()
-    } else {
-        autoHide()
-    }
-}
-
-interface DrawerMap {
-    settingsDrawer: boolean
-    contensDrawer: boolean
-}
-const drawerMap = ref<DrawerMap>({
-    settingsDrawer: false,
-    contensDrawer: false,
-})
-function showDrawer(key: keyof DrawerMap) {
-    drawerMap.value[key] = true
-}
-
-// const chapterIdxArr = computed(() => props.curBook.chapterArr.map(chapter => chapter.idx))
 
 function changeFontSize(type: string) {
     style.value['font-size'] = parseFloat(style.value['font-size']) + (type === 'add' ? 0.1 : -0.1) + 'em'
@@ -136,57 +34,22 @@ function changeFontSize(type: string) {
 const style = ref({
     'font-size': '1em'
 })
+
+const utils = {
+    curBook: props.curBook,
+    moveWindow,
+    changeFontSize,
+    closeReader
+}
 </script>
 <template>
-    <template v-if="headerVisible && !operatePanelVisible">
-        <img src="../assets/Close.svg" @click="back" class="svg-btn-small border close">
-        <img src="../assets/Operate.svg" @click="changeOperatePanelVisible" class="svg-btn-small border operate">
-    </template>
-    <div v-else-if="operatePanelVisible" class="operatePanel">
-        <div class="menu-item flex-r-sbc" @click="showDrawer('contensDrawer')">
-            <span class="menu-name">Contents - 100%</span>
-            <img src="../assets/Bars3.svg" class="svg-btn">
-        </div>
-        <div class="menu-item flex-r-sbc">
-            <span class="menu-name">Search Book</span>
-            <img src="../assets/Search.svg" class="svg-btn">
-        </div>
-        <div class="menu-item flex-r-sbc" @click="showDrawer('settingsDrawer')">
-            <span class="menu-name">Themes & Settings</span>
-            <img src="../assets/Setting.svg" class="svg-btn">
-        </div>
-        <div class="menu-item flex-r-sbc none-decoration">
-            <img src="../assets/Search.svg" class="louma svg-btn">
-            <img src="../assets/Search.svg" class="louma svg-btn">
-            <img src="../assets/Search.svg" class="louma svg-btn">
-        </div>
-    </div>
+    <ReaderUI :utils="utils" />
     <article class="reader">
-        <a href="www.baidu.com">clickme</a>
-        <main :style="style" @click="() => operatePanelVisible ? changeOperatePanelVisible() : changeHeaderVisible()">
-            <template v-for="(para, idx) in vList">
-                <!-- <h3 v-if="chapterIdxArr.indexOf(idx) !== -1">{{ para }}</h3> -->
-                <!-- <p v-else>{{ para }}</p> -->
+        <main :style="style">
+            <template v-for="para in vList">
                 <p>{{ para }}</p>
-                <div v-if="idx === vList.length / 2" id="anchor">
-                </div>
             </template>
         </main>
-        <!-- <footer>footer</footer> -->
-        <Drawer v-model="drawerMap.settingsDrawer" title="Themes & Settings">
-            <div class="fontsize-adjust-btn flex-r-sbc">
-                <span class="left-letter" @click="changeFontSize('sub')">A</span>
-                <span class="divide"></span>
-                <span class="right-letter" @click="changeFontSize('add')">A</span>
-            </div>
-        </Drawer>
-        <Drawer v-model="drawerMap.contensDrawer" title="Contents" height="80vh">
-            <ul class="contents">
-                <li v-for="(chapter, idx) in curBook.chapterArr" :key="idx">
-                    <a style="color: unset;">{{ chapter.content }}</a>
-                </li>
-            </ul>
-        </Drawer>
     </article>
 </template>
 
@@ -206,109 +69,6 @@ const style = ref({
         text-align: left;
         -webkit-touch-callout: none;
     }
-}
-
-.svg-btn-small {
-    height: 1.5em;
-    width: 1.5em;
-    cursor: pointer;
-
-    &.border {
-        border: 0.25rem solid var(--border-color);
-        border-radius: 50%;
-        background-color: var(--border-color);
-    }
-
-    &.close {
-        position: fixed;
-        right: 1em;
-        top: 2em;
-    }
-
-    &.operate {
-        position: fixed;
-        right: 1em;
-        bottom: 2em;
-        border-radius: 25%;
-    }
-
-}
-
-.operatePanel {
-    position: fixed;
-    right: 0;
-    bottom: 3em;
-    -webkit-user-select: none;
-    user-select: none;
-
-    .menu-item {
-        border-radius: 0.75em;
-        margin: 0.25em 2em;
-        padding: 0.5em 1em;
-        background-color: var(--fill-color);
-        /* box-shadow: var(--box-shadow); */
-        cursor: pointer;
-        width: var(--bar-width);
-
-        .menu-name {
-            font-size: 1.2em;
-            font-weight: bold;
-        }
-
-        &.none-decoration {
-            width: calc(var(--bar-width) + 2em);
-            padding: 0;
-            background-color: unset;
-            box-shadow: none;
-
-            .louma {
-                background-color: var(--fill-color);
-                /* box-shadow: var(--box-shadow); */
-                flex: 1;
-                margin: 0 0.25em;
-                padding: 0.5em;
-                border-radius: 1em;
-
-                &:first-child {
-                    margin-left: 0;
-                }
-
-                &:last-child {
-                    margin-right: 0;
-                }
-            }
-        }
-
-    }
-
-
-}
-
-.fontsize-adjust-btn {
-    padding: 0.5em;
-    background-color: var(--border-color);
-    border-radius: 1em;
-
-    .divide {
-        border: 0.5px solid;
-        height: 1.5em;
-        margin: auto;
-    }
-
-    .left-letter {
-        font-size: 0.8em;
-        flex: 1;
-    }
-
-    .right-letter {
-        font-size: 1.2em;
-        flex: 1;
-    }
-}
-
-.contents {
-    text-align: left;
-    font-size: 1.3em;
 }
 
 @media(max-width: 1280px) {
---
+++ src/components/ReaderUI.vue
@@ -0,0 +1,265 @@
+<script setup lang="ts">
+import { Book } from '../modules/indexDb';
+import Drawer from './Drawer.vue';
+import { ref, onMounted, onUnmounted, watch } from 'vue';
+
+interface Utils {
+    moveWindow: (param: string) => void
+    closeReader: () => void
+    changeFontSize: (param: string) => void
+    curBook: Book
+}
+const props = defineProps<{
+    utils: Utils
+}>()
+
+const { moveWindow, closeReader, changeFontSize, curBook } = props.utils
+
+const curTimeoutID = ref(-1)
+
+function terminate() {
+    curTimeoutID.value > 0 && clearTimeout(curTimeoutID.value)
+    curTimeoutID.value = -1
+}
+function startTimeout() {
+    curTimeoutID.value = setTimeout(() => {
+        curTimeoutID.value = -1
+        subLayer()
+    }, 5000)
+}
+
+enum UILayer {
+    Blank = -1,
+    baseBtns = 0,
+    operatePanel = 1
+}
+const curUILayer = ref<UILayer>(0)
+
+function addLayer() {
+    curUILayer.value += 1
+    judgeLayer(curUILayer.value)
+}
+
+function subLayer() {
+    curUILayer.value -= 1
+    judgeLayer(curUILayer.value)
+}
+
+function judgeLayer(value: UILayer) {
+    // before nextick,so i can terminate timeout if curuilayer !== basebtns
+    // or,start timeout before basebtns been render
+    if (value === UILayer.baseBtns) {
+        startTimeout()
+    } else {
+        terminate()
+    }
+}
+
+interface DrawerMap {
+    settingsDrawer: boolean
+    contensDrawer: boolean
+    searchDrawer: boolean
+}
+const drawerMap = ref<DrawerMap>({
+    settingsDrawer: false,
+    contensDrawer: false,
+    searchDrawer: false
+})
+function showDrawer(key: keyof DrawerMap) {
+    drawerMap.value[key] = true
+}
+
+
+let intervalId = -1
+onMounted(() => {
+    const intersectionObserver = new IntersectionObserver((entries) => {
+        // 如果 intersectionRatio 为 0，则目标在视野外，
+        // 我们不需要做任何事情。
+        // what if pre & next exist in a viewport?
+        if (entries[0].intersectionRatio <= 0) {
+            console.log('trigger,out')
+            return
+        };
+        console.log("Loaded new items", entries);
+    });
+    // 开始监听
+    const anchor = document.querySelector('#anchor')
+    anchor && intersectionObserver.observe(anchor);
+})
+
+
+
+onMounted(() => {
+    startTimeout()
+})
+
+onUnmounted(() => {
+    clearInterval(intervalId)
+})
+</script>
+<template>
+    <div class="panel" @click.self="() => curUILayer > UILayer.Blank ? subLayer() : addLayer()">
+        {{ curTimeoutID }}|{{ curUILayer }}
+        <template v-if="curUILayer === UILayer.baseBtns">
+            <img src="../assets/Close.svg" @click="closeReader" class="svg-btn-small border close">
+            <img src="../assets/Operate.svg" @click="addLayer" class="svg-btn-small border operate">
+        </template>
+        <div v-else-if="curUILayer === UILayer.operatePanel" class="operatePanel">
+            <div class="menu-item flex-r-sbc" @click="showDrawer('contensDrawer')">
+                <span class="menu-name">Contents - 100%</span>
+                <img src="../assets/Bars3.svg" class="svg-btn">
+            </div>
+            <div class="menu-item flex-r-sbc" @click="showDrawer('searchDrawer')">
+                <span class=" menu-name">Search Book</span>
+                <img src="../assets/Search.svg" class="svg-btn">
+            </div>
+            <div class="menu-item flex-r-sbc" @click="showDrawer('settingsDrawer')">
+                <span class="menu-name">Themes & Settings</span>
+                <img src="../assets/Setting.svg" class="svg-btn">
+            </div>
+            <div class="menu-item flex-r-sbc none-decoration">
+                <img src="../assets/Bars3.svg" class="louma svg-btn">
+                <img src="../assets/Search.svg" class="louma svg-btn">
+                <img src="../assets/Setting.svg" class="louma svg-btn">
+            </div>
+        </div>
+        <Drawer v-model="drawerMap.contensDrawer" title="Contents" height="80vh">
+            <ul class="contents">
+                <li v-for="(chapter, idx) in curBook.chapterArr" :key="idx">
+                    <a style="color: unset;">{{ chapter.content }}</a>
+                </li>
+            </ul>
+        </Drawer>
+        <Drawer v-model="drawerMap.searchDrawer" title="Search Book" height="80vh">
+            <!-- <input> -->
+            <div>
+                Coming soon...
+            </div>
+        </Drawer>
+        <Drawer v-model="drawerMap.settingsDrawer" title="Themes & Settings">
+            <div class="fontsize-adjust-btn flex-r-sbc">
+                <span class="left-letter" @click="changeFontSize('sub')">A</span>
+                <span class="divide"></span>
+                <span class="right-letter" @click="changeFontSize('add')">A</span>
+            </div>
+        </Drawer>
+    </div>
+
+</template>
+
+<style scoped>
+.panel {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    overflow: hidden;
+    -webkit-user-select: none;
+    user-select: none;
+}
+
+.svg-btn-small {
+    height: 1.5em;
+    width: 1.5em;
+    cursor: pointer;
+
+    &.border {
+        border: 0.25rem solid var(--border-color);
+        border-radius: 50%;
+        background-color: var(--border-color);
+    }
+
+    &.close {
+        position: fixed;
+        right: 1em;
+        top: 2em;
+    }
+
+    &.operate {
+        position: fixed;
+        right: 1em;
+        bottom: 2em;
+        border-radius: 25%;
+    }
+
+}
+
+.operatePanel {
+    position: fixed;
+    right: 0;
+    bottom: 3em;
+    -webkit-user-select: none;
+    user-select: none;
+
+    .menu-item {
+        border-radius: 0.75em;
+        margin: 0.25em 2em;
+        padding: 0.5em 1em;
+        background-color: var(--fill-color);
+        /* box-shadow: var(--box-shadow); */
+        cursor: pointer;
+        width: var(--bar-width);
+
+        .menu-name {
+            font-size: 1.2em;
+            font-weight: bold;
+        }
+
+        &.none-decoration {
+            width: calc(var(--bar-width) + 2em);
+            padding: 0;
+            background-color: unset;
+            box-shadow: none;
+
+            .louma {
+                background-color: var(--fill-color);
+                /* box-shadow: var(--box-shadow); */
+                flex: 1;
+                margin: 0 0.25em;
+                padding: 0.5em;
+                border-radius: 1em;
+
+                &:first-child {
+                    margin-left: 0;
+                }
+
+                &:last-child {
+                    margin-right: 0;
+                }
+            }
+        }
+
+    }
+
+
+}
+
+.fontsize-adjust-btn {
+    padding: 0.5em;
+    background-color: var(--border-color);
+    border-radius: 1em;
+    cursor: pointer;
+
+    .divide {
+        border: 0.5px solid;
+        height: 1.5em;
+        margin: auto;
+    }
+
+    .left-letter {
+        font-size: 0.8em;
+        flex: 1;
+    }
+
+    .right-letter {
+        font-size: 1.2em;
+        flex: 1;
+    }
+}
+
+.contents {
+    text-align: left;
+    font-size: 1.3em;
+}
+</style>
