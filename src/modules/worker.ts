0bab5db29453efb73e717cdbc83b4acf5e1463a0
Laix <894169287@qq.com>
2024-04-22 14:28:04 +0800

    maybe close?

--- src/modules/worker.ts
+++ src/modules/worker.ts
@@ -41,7 +41,7 @@ function wrapText(canvas: OffscreenCanvasRenderingContext2D, text: string, x: nu
         if (testWidth > maxWidth && n > 0) {
             // debugger
             if ("“”。".includes(testLine[testLine.length - 1]) && testWidth - maxWidth < 9) {
-                console.log(metrics,testLine)
+                console.log(metrics, testLine)
                 line = testLine;
             } else {
                 canvas.fillText(line, x, y);
@@ -63,6 +63,7 @@ addEventListener('message', (e) => {
 
 
 addEventListener('message', (evt) => {
+    return
     const canvas: OffscreenCanvas = evt.data.canvas;
     // console.log(canvas)
     canvas.height = 4000
@@ -130,3 +131,141 @@ function mm() {
             console.log(i, a[i], b[i], a[i] > b[i])
     }
 }
+
+interface Config {
+    maxWidth: number,
+    lineHeight: number,
+    fontSize: number
+}
+
+function measureHeight(canvas: OffscreenCanvasRenderingContext2D, text: string, config: Config) {
+    const { measureText } = canvas
+    const { maxWidth, lineHeight, fontSize } = config
+    const closeWidth = fontSize * 2 //close to linebreak point
+    const length = text.length
+    let resLength = length
+    let res = 0
+
+    // single line
+    if (measureText(text).width < maxWidth) {
+        return lineHeight * fontSize
+    }
+
+    while (resLength > 0) {
+        let left = length - resLength, right = resLength - 1
+        // finf break
+        while (left < right) {
+            const mid = (right - left) / 2 + left
+            const { width: measureWidth } = measureText(text.slice(left, mid))
+
+            const sub = measureWidth - maxWidth
+            if (Math.abs(sub) > closeWidth) {
+                if (sub > 0) right = mid
+                else left = mid
+            } else {
+                // close to break point
+                let offset = 1
+                if (sub > 0) {
+                    for (; mid - offset > left && measureWidth - measureText(text.slice(left, mid - offset)).width <= 0; offset++);
+                } else {
+                    for (; mid + offset < right && measureWidth - measureText(text.slice(left, mid + offset)).width <= 0; offset++);
+                }
+
+                // next 
+                res += lineHeight * fontSize
+                resLength -= mid + sub > 0 ? -offset : offset - left
+            }
+        }
+    }
+
+    return res
+}
+
+
+interface Config {
+    maxWidth: number,
+    lineHeight: number,
+    fontSize: number,
+    fontFamily: string
+}
+
+function measureHeight1(ctx: OffscreenCanvasRenderingContext2D, text: string, config: Config) {
+
+    const { maxWidth, lineHeight, fontSize, fontFamily } = config
+    ctx.font = `${fontSize}px ${fontFamily}`;
+    const closeWidth = fontSize * 2 //close to linebreak point
+    const length = text.length
+
+    const step = Math.ceil(maxWidth / fontSize)
+    let p = 0
+    let q = 0
+
+    let lineCount = 0
+    // debugger
+    while (q < length) {
+        q += step
+        const { width: measureWidth } = ctx.measureText(text.slice(p, q))
+        const sub = measureWidth - maxWidth
+
+
+        if (sub > 0) {
+            let initQ = q
+            while (q > p) {
+                const malou = ctx.measureText(text.slice(--q, initQ))
+                if (malou.width > sub) {
+                    break
+                }
+            }
+            lineCount++
+        } else {
+            let initQ = q
+            while (q < length) {
+                const malou = ctx.measureText(text.slice(initQ, ++q))
+                if (malou.width > Math.abs(sub)) {
+                    break
+                }
+            }
+            lineCount++
+        }
+        p = q
+    }
+    // debugger
+    return lineCount * fontSize * lineHeight
+}
+
+function measureHeight2(ctx: OffscreenCanvasRenderingContext2D, text: string, config: Config) {
+    const { maxWidth, lineHeight, fontSize, fontFamily } = config
+    ctx.font = `${fontSize}px ${fontFamily}`;
+    const { measureText } = ctx
+    const closeWidth = fontSize * 2 //close to linebreak point
+
+    const { width: measureWidth } = measureText(text.slice(p, q))
+}
+
+addEventListener('message', (evt) => {
+    const canvas: OffscreenCanvas = evt.data.canvas;
+    canvas.height = 4000
+    canvas.width = 430
+    const ctx: OffscreenCanvasRenderingContext2D | null = canvas.getContext("2d");
+    if (!ctx) return
+
+    const config: Config = {
+        fontSize: 16,
+        fontFamily: 'inter',
+        lineHeight: 1.5,
+        maxWidth: 430
+    }
+    let paras = allBooks[0].paraArr.slice(0, 10000)
+    let cnt: Array<number> = []
+    let height = config.fontSize
+    console.time()
+    paras.forEach((para: string) => {
+        let cur = measureHeight1(ctx, para, config)
+        height += cur
+        cnt.push(cur)
+    });
+    console.timeEnd()
+    let sum = 0
+    cnt.forEach(i => sum += i)
+    console.log(cnt.reduce((pre, cur) => cur += pre), sum, cnt)
+})
