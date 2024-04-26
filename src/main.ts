8aa562b50644c1868c67ff1b9bcabd3994a600f2
Laix <894169287@qq.com>
2024-04-26 11:47:02 +0800

    remove superfluous code

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-function useWorker() {
-    const myWorker = new Worker("/src/modules/worker1");
-    myWorker.onmessage = (ev) => {
-        console.log(ev.data)
-    }
-
-    const htmlCanvas = document.createElement("canvas");
-    htmlCanvas.width = 430
-    htmlCanvas.height = 930
-    const offscreen = htmlCanvas?.transferControlToOffscreen();
-
-    setTimeout(() => {
-        myWorker.postMessage({
-            canvas: offscreen
-        }, [offscreen]);
-    }, 2000)
-}
-
-
 createApp(App).mount('#app')
